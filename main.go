16a4ace5344b4c56b7986c9440fe6befc6b2e699
178inaba <178inaba@users.noreply.github.com>
2016-11-14 01:24:55 +0900

    Add hatena bookmark ranking 1st to send line.

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package hello
 
 import (
+	"fmt"
 	"net/http"
 
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
 	"google.golang.org/appengine/urlfetch"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gin-gonic/gin"
 	"github.com/line/line-bot-sdk-go/linebot"
 
@@ -36,8 +38,8 @@ func routes() *gin.Engine {
 func index(c *gin.Context) {
 	ctx := appengine.NewContext(c.Request)
 
-	lc, err := linebot.New(cf.Secret, cf.Token,
-		linebot.WithHTTPClient(urlfetch.Client(ctx)))
+	client := urlfetch.Client(ctx)
+	lc, err := linebot.New(cf.Secret, cf.Token, linebot.WithHTTPClient(client))
 	if err != nil {
 		log.Errorf(ctx, "%v", err)
 		c.Status(http.StatusInternalServerError)
@@ -56,9 +58,27 @@ func index(c *gin.Context) {
 			continue
 		}
 
-		switch m := e.Message.(type) {
+		switch e.Message.(type) {
 		case *linebot.TextMessage:
-			_, err := lc.ReplyMessage(e.ReplyToken, linebot.NewTextMessage(m.Text)).Do()
+			resp, err := client.Get("http://b.hatena.ne.jp/ranking/daily")
+			if err != nil {
+				log.Errorf(ctx, "%v", err)
+				return
+			}
+			defer resp.Body.Close()
+
+			doc, err := goquery.NewDocumentFromResponse(resp)
+			if err != nil {
+				log.Errorf(ctx, "%v", err)
+				return
+			}
+
+			dom := doc.Find(".entry-list-l .entry-link").First()
+			url, _ := dom.Attr("href")
+			title := dom.Text()
+
+			msg := fmt.Sprintf("%s\n%s", title, url)
+			_, err = lc.ReplyMessage(e.ReplyToken, linebot.NewTextMessage(msg)).Do()
 			if err != nil {
 				log.Errorf(ctx, "%v", err)
 			}
